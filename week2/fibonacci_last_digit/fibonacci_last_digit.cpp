e92f0b1731ebef73d0bf7b4d5c79141a952cc75e
Alberto <avillprojects@gmail>
2016-03-03 21:39:16 +0000

    fibonaccit_last_digit.cpp solution for problem

--- week2/fibonacci_last_digit/fibonacci_last_digit.cpp
+++ week2/fibonacci_last_digit/fibonacci_last_digit.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <vector>
+/*
+Given an integer n, find the last digit of the nth fibonacci number fn.
+f[0] = 0
+f[1] = 1
+for i=2 to n:
+    f[i] = f[i-1] + f[i-2];
+printf( f[n] mod 10 )
+
+Sample 1
+input:  3
+output: 2
+
+Sample 2
+input:  327305
+output: 5
+*/
 
 int get_fibonacci_last_digit(int n) {
-  //write your code here
-  return 0;
+  std::vector<int> fiblist;
+  fiblist.push_back(0);
+  fiblist.push_back(1);  
+ 
+  for ( int i=2; i <= n; i++ ) {
+    fiblist.push_back( (fiblist[i-1] + fiblist[i-2]) % 10 );
+  }
+  return fiblist.back();
 }
 
 int main() {
