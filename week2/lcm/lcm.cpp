f74d5ef27f5e5b0c7e1f51b12da1664d923cea1a
Alberto <avillprojects@gmail>
2016-03-04 17:56:12 +0000

    Solution for lcm problem

--- week2/lcm/lcm.cpp
+++ week2/lcm/lcm.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 
-long long lcm(int a, int b) {
-  //write your code here
-  return a*b;
+/* NOTE:
+    lcm(a,b) = (a * b)/gcd(a,b)
+*/
+int euclidean_gcd(const int a, const int b) {
+  int aval = a;
+  int bval = b;
+  int prime = 1;
+
+  if (bval == 0)
+    return a;
+
+  for (int d = 2; prime > 0; d++) {
+      prime = aval % bval;
+      if ( prime > 0 ) {
+        aval = bval;
+        bval = prime;
+      }
+  }
+  return bval;
+}
+
+long long lcm(const int a, const int b) {
+  long  aval = a;
+  long  bval = b;
+
+  return (long long)(aval * bval)/euclidean_gcd(aval, bval);
 }
 
 int main() {
